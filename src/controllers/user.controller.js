c5b9838234b4f04d2512f51ac4a125623238159b
KanishkTech <138684623+KanishkTech@users.noreply.github.com>
2024-12-29 21:27:30 +0530

    implement user authentication with login and logout functionality; add JWT verification middleware

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,9 +2,9 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { uploadToCloudinary } from "../utils/cloudinary.js";
-import {ApiResponse} from "../utils/ApiResponse.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
-export const registerUser = asyncHandler(async (req, res, next) => {
+const registerUser = asyncHandler(async (req, res, next) => {
   //ALGORITHM
   // get user details from frontend
   // validation - not empty
@@ -17,7 +17,7 @@ export const registerUser = asyncHandler(async (req, res, next) => {
   // return res
 
   const { fullName, email, username, password } = req.body;
-//   console.log(email);
+  //   console.log(email);
 
   if (
     [fullName, email, username, password].some((feild) => feild?.trim() === "") // here we are checking if any of the fields are empty and then we are throwing an error
@@ -40,23 +40,27 @@ export const registerUser = asyncHandler(async (req, res, next) => {
   const avatarLocalPath = req.files?.avatar[0]?.path; //file ki local path access karna
   // const coverLocalPath = req.files?.coverImage[0]?.path;
 
-  let coverLocalPath ; 
+  let coverLocalPath;
 
   // 1) req.files => Ensures that the files property exists in the req object
-  // 2) Array.isArray(req.files.coverImage) => Ensures that the coverImage property is an array 
+  // 2) Array.isArray(req.files.coverImage) => Ensures that the coverImage property is an array
   // Arrays.isArray() is a method that checks if the given value is an array or not.
   // 3) req.files.coverImage.length > 0 => Ensures that the coverImage array has at least one element
-  if(req.files && Array.isArray(req.files.coverImage) &&  req.files.coverImage.length > 0){
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
     coverLocalPath = req.files.coverImage[0].path;
   }
-  if (!avatarLocalPath ) {
+  if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required");
   }
 
   // upload them to cloudinary, avatar
   const avatar = await uploadToCloudinary(avatarLocalPath);
   const coverImage = await uploadToCloudinary(coverLocalPath);
-    // console.log(avatar);
+  // console.log(avatar);
   if (!avatar) {
     throw new ApiError(500, "Avatar upload failed its required field");
   }
@@ -73,17 +77,119 @@ export const registerUser = asyncHandler(async (req, res, next) => {
   const createdUser = await User.findById(user._id).select(
     "-password -refreshToken"
   );
-  if(!createdUser){
+  if (!createdUser) {
     throw new ApiError(500, "Something went Wrong while User creation failed");
   }
 
-  return res.status(200).json(
-    new ApiResponse(
-        200,
-        createdUser,
-        "User created successfully",
-
-    )
-  )
-
+  return res
+    .status(200)
+    .json(new ApiResponse(200, createdUser, "User created successfully"));
 });
+
+const AccessTokenAndRefreshToken = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validationBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "Something went wrong while generating refresh  tokens and access tokens"
+    );
+  }
+};
+
+const loginUser = asyncHandler(async (req, res, next) => {
+  // req body -> data
+  // username or email
+  //find the user
+  //password check
+  //access and referesh token
+  //send cookie
+
+  const { email, username, password } = req.body;
+
+  if (!email || !username) {
+    throw new ApiError(400, "Email or username is required");
+  }
+
+  const user = await User.findOne({
+    $or: [{ email }, { username }],
+  });
+
+  if (!user) {
+    throw new ApiError(400, "User not found");
+  }
+
+  const isPasswordMatch = await user.comparePassword(password);
+
+  if (!isPasswordMatch) {
+    throw new ApiError(400, "Invalid credentials");
+  }
+
+  const { accessToken, refreshToken } = await AccessTokenAndRefreshToken(
+    user._id
+  );
+
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+  //.select("-password -refreshToken") ka matlab hai result se "password" aur "refreshToken" fields ko exclude karna (nahi dena).
+
+  // These options are used to set cookies securely, ensuring the tokens (like accessToken or refreshToken) cannot be easily accessed or modified by the user in the frontend.  const option = {
+  const option = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .cookie("refreshToken", refreshToken, option)
+    .cookie("accessToken", accessToken, option)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+          accessToken,
+          refreshToken,
+        },
+        "User logged in successfully"
+      )
+    );
+});
+
+const logoutUser = asyncHandler(async (req, res, next) => {
+  // get the user
+  // remove the refresh token
+  // clear the cookies
+
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        refreshToken: undefined,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+  const option = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .clearCookie("refreshToken", option)
+    .clearCookie("accessToken", option)
+    .json(new ApiResponse(200, {}, "User logged out successfully"));
+});
+
+export { registerUser, loginUser, logoutUser };
---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,34 @@
+import { asyncHandler } from "../utils/asyncHandler";
+import { ApiError } from "../utils/ApiError";
+import { User } from "../models/User";
+import jwt from "jsonwebtoken";
+
+const verifyJwt = asyncHandler(async (req, _ , next) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.headers("Authorization"?.replace("Bearer ", ""));
+    //req.header ye mobile browser me use hota hai agar user mobile se kuch kar raha ho to
+
+    if (!token) {
+      throw new ApiError(401, "Unauthorized request");
+    }
+
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const user = await User.findById(decoded?._id).select(
+      "-password -refreshToken"
+    );
+
+    if (!user) {
+      throw new ApiError(401, "invalid Access Token");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid Access Token");
+  }
+});
+
+export { verifyJwt };
